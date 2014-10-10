0a1dc997955e6d75abeccad952030f786fe9678e
skoka <maranellored@gmail.com>
2014-10-09 23:37:39 -0700

    Initial experiments

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/rdwilliamson/aws"
+	"github.com/rdwilliamson/aws/glacier"
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+
+	fmt.Println("Enter absolute path of file to pick up AWS credentials from:")
+	absPath := ""
+	fmt.Scanln(&absPath)
+
+	credsMap, err := getCredentials(absPath)
+	if err != nil {
+		panic(err)
+	}
+	accessKey := credsMap["access-key"]
+	secretKey := credsMap["secret-key"]
+
+	connection := glacier.NewConnection(secretKey, accessKey, aws.USEast1)
+
+	err = connection.CreateVault("Example")
+	if err != nil {
+		panic(err)
+	}
+
+	vault, err := connection.DescribeVault("Example")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(vault)
+
+}
+
+func getCredentials(filePath string) (map[string]string, error) {
+	buffer, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		errors.New("Cannot open file for reading")
+	}
+
+	credsMap := make(map[string]string)
+	err = yaml.Unmarshal(buffer, credsMap)
+	if err != nil {
+		return nil, errors.New("Cannot unmarshal data from file")
+	}
+
+	return credsMap, err
+}
